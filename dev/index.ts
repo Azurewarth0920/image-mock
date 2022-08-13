2aa5df14224301f7ec93cbb357bc5d9443a81d1c
pei.fan <fanpei920@gmail.com>
2022-08-13 14:46:53 +0900

    fix: update dev

--- dev/index.ts
+++ dev/index.ts
@@ -1,16 +1,23 @@
 import express from 'express'
-import { server } from '../src'
+import { server } from '../lib'
 import path from 'path'
 
 const app = express()
 const port = 3000
 
 app.use(express.static(path.resolve(__dirname, './public')))
-app.get('/dummy-image', (_, res) => {
+app.get('/dummy', (_, res) => {
   res.send(
     server({
       width: 300,
       height: 300,
+      color: '#000',
+      imageType: 'jpeg',
+      text: 'Dummy ImageImage Image',
+      backgroundColor: '#0c83eb',
+      font: 'Times',
+      fontSize: 32,
+      borderRadius: 50,
     }).toBuffer()
   )
 })
