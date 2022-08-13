8a4ca8a3b0f36b939ace8f0ee96fdf3f13b16c7b
pei.fan <fanpei920@gmail.com>
2022-08-13 14:55:51 +0900

    feat: add express example

---
+++ example/express/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const { server } = require('../../../dist/index.js')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.get('/dummy-image', (req, res) => {
+  res.send(
+    server({
+      width: 200,
+      height: 200,
+      text: 'Buffer',
+      imageType: 'png',
+    }).toBuffer() // response with file buffer.(png type)
+  )
+})
+
+app.get('/dummy-image/base64', (req, res) => {
+  res.send(
+    server({
+      width: 200,
+      height: 200,
+      text: 'Base64',
+    }).toBase64() // response with base64 string.
+  )
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
